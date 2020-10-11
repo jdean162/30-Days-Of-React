76db03a0cec19f2510a24cdc3ab333aa24b2debe
James Dean <jdean162@gmail.com>
2020-10-11 21:57:52 +0200

    Create my react project

---
+++ my-reacte/src/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const header = (
+    <header>
+        <div className='header-wrapper'>
+            <h1>Welcome to 30 Days Of React</h1>
+            <h2>Getting Started React</h2>
+            <h3>JavaScript Library</h3>
+            <p>Instructor: Asabeneh Yetayeh</p>
+            <small>Date: Oct 1, 2020</small>
+        </div>
+    </header>
+)
+
+// JSX element, main
+const main = (
+    <main>
+        <div className='main-wrapper'>
+            <p>
+                Prerequisite to get started{' '}
+                <strong>
+                    <em>react.js</em>
+                </strong>
+                :
+            </p>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li> JavaScript</li>
+            </ul>
+        </div>
+    </main>
+)
+
+// JSX element, footer
+const footer = (
+    <footer>
+        <div className='footer-wrapper'>
+            <p>Copyright 2020</p>
+        </div>
+    </footer>
+)
+
+const app = (
+    <div className='app'>
+        {header}
+        {main}
+        {footer}
+    </div>
+)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(app, rootElement)
