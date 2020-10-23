82181edea8892109e050aa1d25cb70b8771acdb7
James Dean <jdean162@gmail.com>
2020-10-23 14:03:00 +0200

    Function prop types

--- 05_Day_Props/05_props_boilerplate/src/index.js
+++ 05_Day_Props/05_props_boilerplate/src/index.js
@@ -17,7 +17,62 @@ const Skills = (props) => {
     return <ul>{skills}</ul>
 }
 
+const showTime = (time) => {
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
+
+    const month = months[time.getMonth()]
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return `${date} ${month} ${year}`
+
+}
+
+const Header = (props) => (
+    <header>
+        <div className='header-wrapper'>
+            <h1>{props.data.welcome}</h1>
+            <h2>{props.data.title}</h2>
+            <h3>{props.data.subtitle}</h3>
+            <p>{props.data.author.firstName} {props.data.author.lastName}</p>
+            <small>{showTime(props.data.date)}</small>
+        </div>
+    </header>
+)
+
+const Button = (props) => (
+    <button onClick={props.onClick}>{props.text}</button>
+)
+
 const App = () => {
+
+    const sayHi = () => {
+        console.log('hi')
+    }
+
+    const data = {
+        welcome: 'Welcome to 30 Days Of React',
+        title: 'Getting Started React',
+        subtitle: 'JavaScript Library',
+        author: {
+            firstName: 'James',
+            lastName: 'Dean'
+        },
+        date: new Date()
+    }
+
     let weight = '60.5'
     let currentYear = 2020
     let birthYear = 1992
@@ -28,10 +83,14 @@ const App = () => {
 
     return (
         <div className='app'>
+            <Header data={data}/>
             <Age age={age}/>
             <Weight weight={weight}/>
             <Status status={status}/>
             <Skills skills={skills}/>
+            <button onClick={sayHi}>Say Hi</button>
+
+            <Button text='Say Hi 2!' onClick={() => alert('test')}/>
         </div>
     )
 }
